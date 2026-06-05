b2cc88cafdd8951844cbc9e85c5ce9363f40b291
Aniele Rodrigues <anielerodrigues19@gmail.com>
2026-06-05 18:40:55 -0300

    Add files via upload

---
+++ Comentario.java
@@ -0,0 +1,13 @@
+public class Comentario {
+    public static void main(String[] args) {
+
+        System.err.println("Connecting to server…");   //o programa tenta estabelecer uma coneção com um servidor//
+        System.err.println("Error: Connection timed out.");  //A tentativa de coneção com o servidor falhou//
+        System.err.println("Retrying…");   // O programa será reiniciado.//
+
+
+    }
+
+}
+    
+
